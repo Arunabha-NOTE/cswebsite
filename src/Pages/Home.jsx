177801259d685b87f32b18e1e8623020181f49b3
Arunabha-NOTE <arunabham2017@gmail.com>
2023-09-21 01:36:44 +0530

    template created

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import Navbar from "../Components/Navbar";
+import globalbg from "../Assets/HomePageVideo/chipdesign.png"
 const Home = () => {
     return (
-        <div>
+        <>
+            <div>
+                <div className='font-mono text-ieeewhite bg-ieeegrey'>
+                    <div className='font-mono text-dracxt h-auto bg-dracula sticky top-0 z-20'>
+                        <div className='bg-pracula'>
+                            <Navbar />
+                        </div>
+                    </div>
+                    <div style={{backgroundImage: `url(${globalbg})`}} className='h-[90vh] lg:bg-fixed bg-cover bg-no-repeat'>
+                    </div>
+                    <div className='mx-[5vw] lg:mx-[10vw]'>
 
-        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
